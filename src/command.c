f356264493a42779ac31471d7d09e0a2a7824193
jakos <jakosvadim@gmail.com>
2024-02-08 17:13:43 +0300

    Add help command

--- src/command.c
+++ src/command.c
@@ -10,10 +10,95 @@
 #include <getopt.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdarg.h>
+
+#define HELP "`slist [-f file] <command> <task> <section>`\n"\
+    "`slist` show all sections\n"\
+    "`slist [section_pos|section_name]` show section\n"\
+    "`slist add [task]` add task to default section\n"\
+    "`slist add [task] [section_pos|section_name]` add task to section\n"\
+    "`slist del [task_pos] [section_pos|section_name] delete task\n"\
+    "`slist mv [task_pos] [section_pos|section_name] [target_section]\n"
+    
 
 #define ADD_CMD "add"
 #define MOVE_CMD "mv"
 #define DELETE_CMD "del"
+#define HELP_CMD "help"
+
+static void command_error(const char *err_fmt, ...)
+{
+    va_list vl;
+    va_start(vl, err_fmt);
+    vfprintf(stderr, err_fmt, vl);
+    va_end(vl);
+
+    exit(1);
+}
+
+static int str_to_pos(const char *str_pos)
+{
+    int pos;
+    pos = atoi(str_pos);
+    return pos;
+}
+
+static void show_section(const struct section_dbl_node *section)
+{
+    struct item_dbl_node *item;
+    int pos;
+
+    printf("==== %s ====\n", section->section_name);
+    pos = 1;
+    for (item = section->items->first; item; item = item->next) {
+        printf("[%d] %s\n", pos, item->item_name);
+        pos += 1;
+    }
+}
+
+static void show_all_sections(const struct section_dbl *sections)
+{
+    int pos;
+    struct section_dbl_node *section;
+
+    pos = 1;
+    puts("==== Sections ====");
+    for (section = sections->first; section; section = section->next) {
+        printf("[%d] %s\n", pos, section->section_name);
+        pos += 1;
+    }
+}
+
+static struct item_dbl_node*
+get_item(const char *item_name, struct section_dbl_node *section)
+{
+    struct item_dbl_node *item;
+    int item_pos;
+
+    item_pos = str_to_pos(item_name);
+    item = item_dbl_search_by_pos(item_pos, section->items);
+    if (!item)
+        command_error("Incorrect item position [%d]\n", item_pos);
+
+    return item;
+}
+
+static struct section_dbl_node*
+get_section(const char *section_name, struct section_dbl *sections)
+{
+    struct section_dbl_node *section;
+    if (isdigit(section_name[0])) {
+        int section_pos = str_to_pos(section_name);
+        section = section_dbl_search_by_pos(section_pos, sections);
+        if (!section)
+            command_error("Incorrect section position [%d]\n", section_pos);
+    }
+    else {
+        section = section_dbl_search(section_name, sections);
+    }
+
+    return section;
+}
 
 static void add_item_cmd(const char *item_name, const char *section_name,
                          struct config *cfg)
@@ -22,23 +107,17 @@ static void add_item_cmd(const char *item_name, const char *section_name,
     struct section_dbl_node *section;
     sections = load_sections(cfg->data_location, cfg->default_section);
 
-    if (isdigit(section_name[0])) {
-        section = section_dbl_search_by_pos(atoi(section_name), sections);
-        if (!section) {
-            fprintf(stderr, "Incorrect section position [%d]\n", 
-                   atoi(section_name));
-            exit(1);
-        }
+    section = get_section(section_name, sections);
+    if (!section) {
+        section_dbl_push_back(section_name, sections);
+        section = sections->last;
     }
-    else {
-        section = section_dbl_search(section_name, sections);
-        if (!section) {
-            section_dbl_push_back(section_name, sections);
-            section = sections->last;
-        }
-    }
+    
     item_dbl_push_back(item_name, section->items);
     put_sections(sections, cfg->default_section, cfg->data_location);
+
+    show_section(section);
+
     section_dbl_free(sections);
     free(sections);
 }
@@ -52,49 +131,28 @@ static void move_item_cmd(const char *item_name, const char *section_name,
     struct section_dbl_node *target_section;
 
     sections = load_sections(cfg->data_location, cfg->default_section);
+    section = get_section(section_name, sections);
+    if (!section)
+        command_error("Section \"%s\" not found\n", section_name);
 
-    if (isdigit(section_name[0])) {
-        section = section_dbl_search_by_pos(atoi(section_name), sections);
-        if (!section) {
-            fprintf(stderr, "Incorrect section position [%d]\n", 
-                    atoi(section_name));
-            exit(1);
-        }
-    } else {
-        section = section_dbl_search(section_name, sections);
-        if (!section) {
-            fprintf(stderr, "Section \"%s\" not found\n", section_name);
-            exit(1);
-        }
-    }
+    item = get_item(item_name, section);
 
-    item = item_dbl_search_by_pos(atoi(item_name), section->items);
-    if (!item) {
-        fprintf(stderr, "Incorrect item position [%d]\n", atoi(item_name));
-        exit(1);
-    }
-
-    if (isdigit(target_section_name[0])) {
-        target_section = section_dbl_search_by_pos(atoi(target_section_name),
-                                                   sections);
-        if (!target_section) {
-            fprintf(stderr, "Incorrect section position [%d]\n", 
-                    atoi(target_section_name));
-            exit(1);
-        }
-    } else {
-        target_section = section_dbl_search(target_section_name, sections);
-        if (!target_section) {
-            section_dbl_push_back(target_section_name, sections);
-            target_section = sections->last;
-        }
+    target_section = get_section(target_section_name, sections);
+    if (!target_section) {
+        section_dbl_push_back(target_section_name, sections);
+        target_section = sections->last;
     }
 
     item_dbl_push_back(item->item_name, target_section->items);
     item_dbl_remove(item, section->items);
+
     if (item_dbl_is_empty(section->items))
         section_dbl_remove(section, sections);
     target_section = section_dbl_search(target_section_name, sections);
+
+    show_section(section);
+    show_section(target_section);
+
     put_sections(sections, cfg->default_section, cfg->data_location);
     section_dbl_free(sections);
     free(sections);
@@ -109,30 +167,22 @@ static void delete_item_cmd(const char *item_name, const char *section_name,
 
     sections = load_sections(cfg->data_location, cfg->default_section);
 
-    if (isdigit(section_name[0])) {
-        section = section_dbl_search_by_pos(atoi(section_name), sections);
-        if (!section) {
-            fprintf(stderr, "Incorrect section position [%d]\n", 
-                    atoi(section_name));
-            exit(1);
-        }
-    } else {
-        section = section_dbl_search(section_name, sections);
-        if (!section) {
-            fprintf(stderr, "Section \"%s\" not found\n", section_name);
-            exit(1);
-        }
-    }
+    section = get_section(section_name, sections);
+    if (!section)
+        command_error("Section \"%s\" not found\n", section_name);
 
-    item = item_dbl_search_by_pos(atoi(item_name), section->items);
-    if (!item) {
-        fprintf(stderr, "Incorrect item position [%d]\n", atoi(item_name));
-        exit(1);
-    }
+    item = get_item(item_name, section);
 
     item_dbl_remove(item, section->items);
-    if (item_dbl_is_empty(section->items))
+    if (item_dbl_is_empty(section->items) &&
+            strcmp(section->section_name, cfg->default_section) != 0) 
+    {
+        printf("Section \"%s\" has been removed\n", section->section_name);
         section_dbl_remove(section, sections);
+        show_all_sections(sections);;
+    } else {
+        show_section(section);
+    }
 
     put_sections(sections, cfg->default_section, cfg->data_location);
     section_dbl_free(sections);
@@ -141,28 +191,16 @@ static void delete_item_cmd(const char *item_name, const char *section_name,
 
 static void show_section_cmd(const char *section_name, struct config *cfg)
 {
-    int pos;
     struct section_dbl *sections;
     struct section_dbl_node *section;
-    struct item_dbl_node *item;
 
     sections = load_sections(cfg->data_location, cfg->default_section);
 
-    if (isdigit(section_name[0]))
-        section = section_dbl_search_by_pos(atoi(section_name), sections);
-    else
-        section = section_dbl_search(section_name, sections);
-    if (!section) {
-        fprintf(stderr, "Section \"%s\" not found\n", section_name);
-        exit(1);
-    }
+    section = get_section(section_name, sections);
+    if (!section)
+        command_error("Section \"%s\" not found\n", section_name);
 
-    printf("==== %s ====\n", section->section_name);
-    pos = 1;
-    for (item = section->items->first; item; item = item->next) {
-        printf("[%d] %s\n", pos, item->item_name);
-        pos += 1;
-    }
+    show_section(section);
     section_dbl_free(sections);
     free(sections);
 }
@@ -170,16 +208,9 @@ static void show_section_cmd(const char *section_name, struct config *cfg)
 static void show_all_sections_cmd(struct config *cfg)
 {
     struct section_dbl *sections;
-    struct section_dbl_node *section;
-    int pos;
 
-    pos = 1;
     sections = load_sections(cfg->data_location, cfg->default_section);
-    puts("==== Sections ====");
-    for (section = sections->first; section; section = section->next) {
-        printf("[%d] %s\n", pos, section->section_name);
-        pos += 1;
-    }
+    show_all_sections(sections);
     section_dbl_free(sections);
     free(sections);
 }
@@ -187,12 +218,17 @@ static void show_all_sections_cmd(struct config *cfg)
 void perform_command(int argc, char **argv, struct config *cfg)
 {
     char *cmd;
+    
     switch (argc) {
     case 0:
         show_all_sections_cmd(cfg);
         break;
     case 1: 
-        show_section_cmd(argv[0], cfg);
+        if (strcmp(argv[0], HELP_CMD) == 0) {
+            printf(HELP);
+        } else {
+            show_section_cmd(argv[0], cfg);
+        }
         break;
     default:
         cmd = argv[0];
@@ -202,22 +238,16 @@ void perform_command(int argc, char **argv, struct config *cfg)
         if (strcmp(cmd, ADD_CMD) == 0) {
             puts(argv[0]);
             if (argc > 1) {
-                puts(argv[1]);
                 add_item_cmd(argv[0], argv[1], cfg);
-                show_section_cmd(argv[1], cfg);
             } else {
                 add_item_cmd(argv[0], cfg->default_section, cfg);
-                show_section_cmd(cfg->default_section, cfg);
             }
         } else
         if (strcmp(cmd, MOVE_CMD) == 0) {
             move_item_cmd(argv[0], argv[1], argv[2], cfg);
-            show_section_cmd(argv[1], cfg);
-            show_section_cmd(argv[2], cfg);
         } else
         if (strcmp(cmd, DELETE_CMD) == 0) {
             delete_item_cmd(argv[0], argv[1], cfg); 
-            show_section_cmd(argv[1], cfg);
         }
     }
     free(cfg);
